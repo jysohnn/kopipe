3e0010db6f03ea345f42e31dfa58839e82c5de17
jysohnn <jysohnn@gmail.com>
2025-08-17 04:12:16 +0900

    feat: OpenAIEmbeddingVectorStore embedding vector 변환 API 호출부에 예외 처리 추가

--- src/main/kotlin/io/github/jysohnn/kopipe/knowledge/OpenAIEmbeddingVectorStore.kt
+++ src/main/kotlin/io/github/jysohnn/kopipe/knowledge/OpenAIEmbeddingVectorStore.kt
@@ -40,18 +40,18 @@ class OpenAIEmbeddingVectorStore(
     private val retrievingHistory: MutableSet<String> = mutableSetOf()
 
     override fun addAll(knowledge: List<String>) {
-        val embeddingVectors = toEmbeddingVectors(texts = knowledge).mapIndexed { index, vector ->
+        val embeddingVectors = toEmbeddingVectors(texts = knowledge)?.mapIndexed { index, vector ->
             EmbeddingVector(
                 vector = vector,
                 text = knowledge[index]
             )
-        }
+        } ?: emptyList()
 
         this.embeddingVectors.addAll(embeddingVectors)
     }
 
     override fun retrieve(query: String, minSimilarity: Double): String? {
-        val queryVector = toEmbeddingVector(text = query)
+        val queryVector = toEmbeddingVector(text = query) ?: return null
         val mostSimilarVector = this.embeddingVectors.maxBy {
             calculateCosineSimilarity(queryVector, it.vector)
         }
@@ -63,33 +63,37 @@ class OpenAIEmbeddingVectorStore(
         return mostSimilarVector.text
     }
 
-    private fun toEmbeddingVector(text: String): List<Double> {
-        return toEmbeddingVectors(listOf(text))[0]
+    private fun toEmbeddingVector(text: String): List<Double>? {
+        return toEmbeddingVectors(listOf(text))?.get(0)
     }
 
-    private fun toEmbeddingVectors(texts: List<String>): List<List<Double>> {
+    private fun toEmbeddingVectors(texts: List<String>): List<List<Double>>? {
         if (texts.isEmpty()) return emptyList()
 
-        val requestObject = OpenAIEmbeddingRequest(
-            model = model,
-            encodingFormat = "float",
-            input = texts
-        )
-        val requestBody = objectMapper.writeValueAsString(requestObject).toRequestBody()
+        try {
+            val requestObject = OpenAIEmbeddingRequest(
+                model = model,
+                encodingFormat = "float",
+                input = texts
+            )
+            val requestBody = objectMapper.writeValueAsString(requestObject).toRequestBody()
 
-        val request = Request.Builder()
-            .url("https://api.openai.com/v1/embeddings")
-            .addHeader("Content-Type", "application/json")
-            .addHeader("Authorization", "Bearer $apiKey")
-            .post(requestBody).build()
+            val request = Request.Builder()
+                .url("https://api.openai.com/v1/embeddings")
+                .addHeader("Content-Type", "application/json")
+                .addHeader("Authorization", "Bearer $apiKey")
+                .post(requestBody).build()
 
-        val response = httpClient.newCall(request).execute()
-        val responseObject = objectMapper.readValue(
-            response.body.string(),
-            OpenAIEmbeddingResponse::class.java
-        )
+            val response = httpClient.newCall(request).execute()
+            val responseObject = objectMapper.readValue(
+                response.body.string(),
+                OpenAIEmbeddingResponse::class.java
+            )
 
-        return responseObject.data.map { it.embedding }
+            return responseObject.data.map { it.embedding }
+        } catch (_: Throwable) {
+            return null
+        }
     }
 
     private fun calculateCosineSimilarity(a: List<Double>, b: List<Double>): Double {
